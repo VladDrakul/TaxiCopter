cfa3b5fa0c78f1ce3459856bd64db1f0d5c3e580
Cathbadh <devin.wakefield@gmail.com>
2013-09-10 18:16:38 -0700

    Created a grey background activity, because that was what the copied and pasted code did. yayyyyyyyy

--- TaxiCopter/src/games/taxicopter/GameActivity.java
+++ TaxiCopter/src/games/taxicopter/GameActivity.java
@@ -1,15 +1,24 @@
 package games.taxicopter;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
+import android.opengl.GLSurfaceView;
+import android.os.Bundle;
 import android.view.Menu;
 
 public class GameActivity extends Activity {
 
+	private GLSurfaceView mGLView;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_game);
+		
+		// Create a GLSurfaceView instance and set it
+        // as the ContentView for this Activity.
+        mGLView = new MyGLSurfaceView(this);
+        setContentView(mGLView);
 	}
 
 	@Override
@@ -20,3 +29,15 @@ public class GameActivity extends Activity {
 	}
 
 }
+
+class MyGLSurfaceView extends GLSurfaceView {
+
+    public MyGLSurfaceView(Context context){
+        super(context);
+
+        // Set the Renderer for drawing on the GLSurfaceView
+        setRenderer(new MyGL20Renderer());
+    }
+}
+
+
